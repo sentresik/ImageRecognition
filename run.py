ff9786c55bae4183b6be1b1cbe1b309767d5362e
sentresik <sentresik@gmail.com>
2024-05-16 20:01:15 +0200

    run.py TODO

---
+++ run.py
@@ -0,0 +1,23 @@
+# zaladowanie bibliotek
+
+
+# 1. zaladowanie danych - zaladowanie wszystkich danych treningowych i testowych
+
+
+# 2. podzial danych - podzielenie danych na zbior testowy i treningowy
+
+
+# 3. normalizacja danych - dostosowanie rozmiaru i normalizacja wartosci piksela z zakresu 0-255 do 0-1
+
+
+# 4. trening modelu - model SVM i trenowanie na zbiorze treningowym
+
+
+# 5. ocena modelu - dane testowe i sprawdzenie wynikow
+
+
+# 6. wizualizacja wynikow - wykresy i dane
+
+
+# 7. petla iteracyjna - powtarzanie i udoskonalanie modelu
+
